aa4d8bc499e77d72ef601ac5b7e2d58543fa286a
abr-ya <gamer05@list.ru>
2020-08-29 11:31:31 +0300

    Storybook work now

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class CustomButton extends Component {
+    render() {
+        return(
+            <button>Custom Button</button>
+        );
+    }
+};
+
+export default CustomButton;
